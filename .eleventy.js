b5356f397c4980035dd421483a8824561b50be6c
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2025-04-28 11:32:28 +0200

    Added base Layout and test text

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function(eleventyConfig) {
+
+  // // 💡 baseurl global verfügbar machen
+  // eleventyConfig.addGlobalData("global", require("./_data/global.json"))
+
+  eleventyConfig.addPassthroughCopy("src/static/style.css")
+  eleventyConfig.addPassthroughCopy("src/static/script.js")
+  eleventyConfig.addPassthroughCopy("src/static/images")
+  eleventyConfig.addPassthroughCopy("src/static/svg")
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml")
+  eleventyConfig.addPassthroughCopy("src/robots.txt")
+  eleventyConfig.addPassthroughCopy("_data")
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+}
